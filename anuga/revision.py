1c47446dbb6725d92aaa5720266ad0ba6b3479f6
Stephen Roberts <stephen.roberts@anu.edu.au>
2025-09-24 21:32:49 +1000

    update version

--- anuga/revision.py
+++ anuga/revision.py
@@ -4,6 +4,6 @@ This file provides the git sha id and commit date for the installed revision of 
 The file is automatically generated and should not be modified manually.
 """
 
-__git_sha__ = "5be21043fd0847270fcc90b62fe6196796efe9cb"
-__git_committed_datetime__ = "2025-09-24 17:35:52+10:00"
-__version__ = "3.2.1rc3"
+__git_sha__ = "c9781320c713aad03bda5863cedcfac217953126"
+__git_committed_datetime__ = "2025-09-24 20:54:33+10:00"
+__version__ = "3.2.1rc9"
