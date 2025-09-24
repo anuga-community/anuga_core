b6180c646588c10773e3bf0f37fe765f04f35355
Stephen Roberts <stephen.roberts@anu.edu.au>
2025-09-24 11:15:04 +1000

    try to run auditwheel in python-publish

--- anuga/revision.py
+++ anuga/revision.py
@@ -4,6 +4,6 @@ This file provides the git sha id and commit date for the installed revision of 
 The file is automatically generated and should not be modified manually.
 """
 
-__git_sha__ = "46e036eebe3846af318ce3c901ec77ed8acc8850"
-__git_committed_datetime__ = "2025-09-23 21:20:04+10:00"
-__version__ = "3.2.1rc1"
+__git_sha__ = "f7e35a458eb7a5a39a8a92a1ad97f48f1cc018db"
+__git_committed_datetime__ = "2025-09-23 21:54:38+10:00"
+__version__ = "3.2.1rc2"
