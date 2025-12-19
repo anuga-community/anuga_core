f6aa44b64ab06f9a4931c12241467ae0df6d0420
Stephen Roberts <stephen.roberts@anu.edu.au>
2025-12-19 20:13:35 +1100

    hard code location of cuda kernels

--- anuga/shallow_water/sw_domain_cupy.py
+++ anuga/shallow_water/sw_domain_cupy.py
@@ -200,7 +200,7 @@ class GPU_interface(object):
         #with open('../cuda_anuga.cu') as f:
 
         #FIXME SR: Obviously need to make this general!
-        with open('/home/cdacapps01/rutvik/anuga_core/anuga/shallow_water/cuda_anuga.cu') as f:
+        with open('/home/851/sgr851/anuga_core/anuga/shallow_water/cuda_anuga.cu') as f:
             code = f.read()
 
         self.mod  = cp.RawModule(code=code, options=("--std=c++17",),
