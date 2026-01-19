35ba50fa4fe48ec8bbd1bf8e725341e72a5773a6
Stephen Roberts <stephen.roberts@anu.edu.au>
2026-01-19 18:04:56 +1100

    tweaking timestepping_stats output

--- anuga/abstract_2d_finite_volumes/generic_domain.py
+++ anuga/abstract_2d_finite_volumes/generic_domain.py
@@ -1267,7 +1267,7 @@ class Generic_Domain(object):
 
 
 
-        msg += f' ({int(walltime() - self.last_walltime):d}s)'
+        msg += f' ({int(walltime() - self.last_walltime):d}s),'
 
 
 
@@ -1275,21 +1275,24 @@ class Generic_Domain(object):
             # Report cpu time since evolve was called
             # (which may be different from the time since the last call to
             # this function)
-            cpu_time = walltime() - self.evolve_start_walltime
-            cpu_time_hhmmss = anuga.seconds_to_hhmmss(int(cpu_time))
-            #msg += f' cpusum ({cpu_time_hhmmss})'
-            
             
             try:
+                cpu_time = walltime() - self.evolve_start_walltime
+                cpu_time_hhmmss = anuga.seconds_to_hhmmss(int(cpu_time))
                 fraction = self.relative_time/self.relative_finaltime
-
-                bar_len = 15
+                bar_len = 10
                 filled = int(bar_len * fraction)
-                cpu_time_ETA_hhmmss = anuga.seconds_to_hhmmss(int(cpu_time/fraction-cpu_time))
+                
                 bar = "#" * filled + "-" * (bar_len - filled)
-                msg += f' |{bar}| [{cpu_time_hhmmss} < {cpu_time_ETA_hhmmss}]'
-                #msg += f' {100*fraction:.2f}%'
-                #msg += f' ETA ({cpu_time_ETA_hhmmss})'
+                #msg += f' |{bar}|'
+
+                cpu_time_ETA_hhmmss = (
+                    anuga.seconds_to_hhmmss(int(cpu_time / fraction - cpu_time))
+                    if fraction > 0 else "??"
+                )
+
+                msg += f' elapsed ({cpu_time_hhmmss}), eta ({cpu_time_ETA_hhmmss})'
+
             except:
                 pass
             self.last_walltime = walltime()
