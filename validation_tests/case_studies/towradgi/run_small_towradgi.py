c2d04aa036f9680651b8a3552ea3abebecb7d25c
Stephen Roberts <stephen.roberts@anu.edu.au>
2026-04-11 14:47:45 +1000

    fix spelling typo

--- validation_tests/case_studies/towradgi/run_small_towradgi.py
+++ validation_tests/case_studies/towradgi/run_small_towradgi.py
@@ -49,6 +49,8 @@ def read_polygon_list(poly_list):
 if myid == 0:
     print('ABOUT to Start Simulation:- Importing Modules')
 
+anuga.set_logfile('run_small_towradgi.log')
+
 if myid == 0 and not os.path.isdir('DEM_bridges'):
     msg = """
 ################################################################################
@@ -71,14 +73,14 @@ verbose = args.verbose
 # --------------------------------------------------------------------------
 
 verbose = False
-yieldstep=1800. # yield evolve loop every 10 seconds
-outputstep=1800. # update sww files every 60 seconds
+yieldstep=120. # yield evolve loop every 120 seconds
+outputstep=1800. # update sww files every 1800 seconds
 finaltime=3600. #83700.
 
 scale = 1 # For coarse mesh set to 10 (135237 triangles), fine mesh set to 1 (256688 triangles)
 maximum_triangle_area = 1000 # This doesn't make much difference for this mesh
 
-# Choices are 1 (openmp) 2 (cupy)
+# Choices are 1 (openmp/cpu) 2 (openmp/gpu offloading)
 multiprocessor_mode = 2
 
 checkpoint_time = max(600/scale, 60)
